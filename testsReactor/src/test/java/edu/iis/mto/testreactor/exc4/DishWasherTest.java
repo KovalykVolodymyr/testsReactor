5ae68bbf5400fa496770165fcd8c949df1f54c2c
Kovalyk.Volodymyr <211646@edu.p.lodz.pl>
2019-06-27 16:18:03 +0200

    Add 5 tests

--- testsReactor/src/test/java/edu/iis/mto/testreactor/exc4/DishWasherTest.java
+++ testsReactor/src/test/java/edu/iis/mto/testreactor/exc4/DishWasherTest.java
@@ -1,15 +1,87 @@
 package edu.iis.mto.testreactor.exc4;
-
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.when;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
 
-
+@RunWith(MockitoJUnitRunner.class)
 public class DishWasherTest {
 
-    @Test
-    public void test() {
-        fail("Not yet implemented");
+    @Mock
+    WaterPump waterPump;
+    @Mock
+    Engine engine;
+    @Mock
+    DirtFilter dirtFilter;
+    @Mock
+    Door door;
+
+
+    private DishWasher washer;
+    private ProgramConfiguration defaultProgram;
+
+    @Before
+    public void setup() {
+        washer = new DishWasher(waterPump, engine, dirtFilter, door);
+        defaultProgram = ProgramConfiguration.builder().withProgram(WashingProgram.ECO).withTabletsUsed(true).build();
     }
 
+
+    @Test
+    public void washerStartAttemptWithClosedDoorShouldResultInError() {
+        when(door.closed()).thenReturn(true);
+
+        RunResult result = washer.start(defaultProgram);
+
+        Assert.assertThat(result.getStatus(), is(Status.DOOR_OPEN_ERROR));
+    }
+    @Test
+    public void washerStartAttemtWithDirtFilterShouldResultInError() {
+        when(dirtFilter.capacity()).thenReturn(30.0);
+        when(door.closed()).thenReturn(false);
+
+        RunResult result = washer.start(defaultProgram);
+
+        Assert.assertThat(result.getStatus(), is(Status.ERROR_FILTER));
+    }
+
+    @Test
+    public void washerRunWithEcoProgramAndTabletsAndWorkingElementsShouldSucceed() {
+        when(dirtFilter.capacity()).thenReturn(60.0);
+        when(door.closed()).thenReturn(false);
+
+        RunResult result = washer.start(defaultProgram);
+
+        assertThat(result.getStatus(), is(Status.SUCCESS));
+    }
+
+    @Test
+    public void washerShouldUseWaterPumpDrainTwiceWhileLaunchedWithNotRinseProgram() throws PumpException {
+        when(dirtFilter.capacity()).thenReturn(60.0);
+        when(door.closed()).thenReturn(false);
+
+        RunResult result = washer.start(defaultProgram);
+
+        Mockito.verify(waterPump, times(2)).drain();
+    }
+    @Test
+    public void washerShouldUseEngineOnlyOnceWhenLaunchedWithRinseProgram() throws EngineException {
+        when(dirtFilter.capacity()).thenReturn(60.0);
+        when(door.closed()).thenReturn(false);
+
+        ProgramConfiguration rinseProgram = ProgramConfiguration.builder().withProgram(WashingProgram.RINSE).withTabletsUsed(true).build();
+
+        RunResult result = washer.start(rinseProgram);
+
+        Mockito.verify(engine).runProgram(rinseProgram.getProgram().getTimeInMinutes());
+
+    }
 }
